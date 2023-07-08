43656f4c288183c9bfddb41f1ea4321f094c07a7
owaissyed65 <owaissyed65@gmail.com>
2023-07-08 19:36:14 +0500

    adding scroll up

--- src/components/ScrollUp.jsx
+++ src/components/ScrollUp.jsx
@@ -5,8 +5,9 @@ import Div from './Div';
 const ScrollUp = () => {
     const cond = topScroll()
     return cond && (
-        <Div className={`fixed bottom-7 right-[4%] text-blue-800 ${!cond && "hidden"}  `}>
-            <div className='w-[50px] h-[50px] rounded-full bg-[#78E9C5] flex items-center justify-center cursor-pointer' onClick={() => scrollTo('hero')}><AiOutlineArrowUp className='text-white ' size={24} /></div>
+        <Div className={`fixed bottom-7 right-[4%] text-blue-800 ${!cond && "hidden"}  flex justify-center items-center`}>
+            <div className='absolute w-[70px] h-[70px] bg-black/[0.2] rounded-full blur-lg'></div>
+            <div className='w-[50px] h-[50px] rounded-full bg-[#78E9C5] flex items-center justify-center cursor-pointer relative' onClick={() => scrollTo('hero')}><AiOutlineArrowUp className='text-white font-medium' size={24} /></div>
         </Div>
     )
 }
